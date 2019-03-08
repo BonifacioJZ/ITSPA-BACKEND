03adeca1b417039bfee162a5afbb8e20596fce86
Bonifacio Juarez Ceja <revanjz6@gmail.om>
2019-03-08 08:40:56 -0600

    inicio de la configuracion back-end

--- projectoitic/itic/models.py
+++ projectoitic/itic/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# Modelo para la tabla maestros
+class Teacher(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido_paterno = models.CharField(max_length=50)
+    apellido_materno = models.CharField(max_length=50)
+    email = models.EmailField( max_length=254)
+    
