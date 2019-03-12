f0c2bf873e9ad104cb77ec88f537ffaa49e474be
Bonifacio Juzrez Ceja <revanjz6@gmail.com>
2019-03-12 07:31:25 -0600

    Configuracion de bd y renombramiento de carpetas

--- src/projectoitic/settings.py
+++ src/projectoitic/settings.py
@@ -75,18 +75,18 @@ WSGI_APPLICATION = 'projectoitic.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-    #'default':{
-    #    'ENGINE': 'django.db.backends.postgresql',
-    #    'NAME': 'mydatabase',
-    #    'USER': 'mydatabaseuser',
-    #    'PASSWORD': 'mypassword',
-    # 'HOST': '127.0.0.1',
-    #  'PORT': '5432' 
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     #}
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'itic',
+        'USER': 'root',
+        'PASSWORD': 'Ghost6699',
+        'HOST': '127.0.0.1',
+        'PORT': '3306' 
+    }
 }
 
 
