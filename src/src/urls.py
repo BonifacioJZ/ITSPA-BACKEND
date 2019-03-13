02faa83401529f85e08c73aa372ff454eebee6c4
Bonifacio Juzrez Ceja <revanjz6@gmail.com>
2019-03-13 08:07:47 -0600

    Commit de urls

--- src/src/urls.py
+++ src/src/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('itic.urls'))    
+    path('api/v1.0/',include('itic.urls'))    
 ]
 
 if settings.DEBUG:
