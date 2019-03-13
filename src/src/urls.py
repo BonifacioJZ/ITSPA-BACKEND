5b298d18689e5140c7fed7532aef25030edc3677
Bonifacio Juzrez Ceja <revanjz6@gmail.com>
2019-03-12 21:55:21 -0600

    api rest

--- src/src/urls.py
+++ src/src/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('itic.urls'))
+    path('api/v1.0',include('itic.urls'))
     
 ]
