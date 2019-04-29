9871e65bb62a3f21f18b8d2010eae073aa5a4804
Bonifacio Juarez Ceja <revanjz6@gmail.com>
2019-04-28 23:35:12 -0500

    configuraciones base de datos

--- src/itspa/admin.py
+++ src/itspa/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Archivo, Proceso
+from .models import Archivo, Proceso,Tag,Noticia
 
 # Register your models here.
 
@@ -13,6 +13,19 @@ class AdminArchivo(admin.ModelAdmin):
     list_display = ["nombre","timestamp"]
     class Meta:
         modle = Archivo
+class AdminNoticia(admin.ModelAdmin):
+    list_display = ["titulo","timestamp"]
+
+    class Meta:
+        modle = Noticia
+class AdminTag(admin.ModelAdmin):
+    list_display=["nombre","timestamp"]
+
+    class Meta:
+        modle = Tag
+
 
 admin.site.register(Proceso,AdminProceso)
 admin.site.register(Archivo,AdminArchivo)
+admin.site.register(Tag,AdminTag)
+admin.site.register(Noticia,AdminNoticia)
